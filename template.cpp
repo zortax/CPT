c642ad536caf6e01a81c7c66190eb1715b62bef0
Jonas Schmidt <mail@schmidt-jonas.de>
2019-11-05 00:14:29 +0100

    changed template

--- template.cpp
+++ template.cpp
@@ -2,22 +2,22 @@
 
 #define rep0(a, b)  for(int a = 0; a < (b); ++a)
 #define rep1(a, b)  for(int a = 1; a < (b); ++a)
+#define rev(a,b)    for(int a = (b)-1; a >= 0; --a)
 #define trav(a, b)  for(auto& a : (b))
 #define all(a)      (a).begin(),(a).end()
 #define endl        '\n'
+#define INF         0x3f3f3f3f
 
 using namespace std;
-using vvi = vector<vector<int>>;
-using vi = vector<int>;
 using ll = int64_t;
+using vvi = vector<vector<int>>;
+using pii = pair<int, int>;
+using vi = vector<int>;
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.precision(10);
-
-
-    //content
 
 
     return 0;
