ead3a09bdf7d6a16a4c8da2c4a34062ef9201746
Jonas Schmidt <mail@schmidt-jonas.de>
2019-11-08 21:17:06 +0100

    template changes

--- template.cpp
+++ template.cpp
@@ -1,19 +1,17 @@
 #include <bits/stdc++.h>
 
-#define rep0(a, b)  for(int a = 0; a < (b); ++a)
-#define rep1(a, b)  for(int a = 1; a < (b); ++a)
-#define rev(a,b)    for(int a = (b)-1; a >= 0; --a)
-#define trav(a, b)  for(auto& a : (b))
-#define all(a)      (a).begin(),(a).end()
-#define endl        '\n'
-#define INF         0x3f3f3f3f
-#define M           
+#define rep(i, a, b)    for(int i = a; i < (b); ++i)
+#define rev(i, a,b)     for(int i = (a)-1; i >= a; --i)
+#define trav(a, b)      for(auto& a : (b))
+#define all(a)          (a).begin(),(a).end()
+#define endl            '\n'
+#define INF             0x3f3f3f3f
 
 using namespace std;
 using ll = int64_t;
-using vvi = vector<vector<int>>;
-using pii = pair<int, int>;
 using vi = vector<int>;
+using vvi = vector<vi>;
+using pii = pair<int, int>;
 
 int main() {
     ios::sync_with_stdio(false);
@@ -23,4 +21,3 @@ int main() {
 
     return 0;
 }
-
