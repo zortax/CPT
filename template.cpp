dc990c512899417e4aaae28a3ff2a769d080eafb
Jonas Schmidt <mail@schmidt-jonas.de>
2019-11-22 21:09:28 +0100

    template changes

--- template.cpp
+++ template.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 
 #define rep(i, a, b)    for(int i = a; i < (b); ++i)
-#define rev(i, a,b)     for(int i = (a)-1; i >= a; --i)
+#define rev(i, a,b)     for(int i = (b)-1; i >= a; --i)
 #define trav(a, b)      for(auto& a : (b))
 #define all(a)          (a).begin(),(a).end()
 #define endl            '\n'
@@ -12,6 +12,7 @@ using ll = int64_t;
 using vi = vector<int>;
 using vvi = vector<vi>;
 using pii = pair<int, int>;
+using vpi = vector<pii>;
 
 int main() {
     ios::sync_with_stdio(false);
