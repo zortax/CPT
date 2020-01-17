132ba11640b2a91c57d8265a1b245289b4771840
Jonas Schmidt <mail@schmidt-jonas.de>
2020-01-17 17:17:36 +0100

    updated template

--- template.cpp
+++ template.cpp
@@ -1,18 +1,20 @@
 #include <bits/stdc++.h>
 
 #define rep(i, a, b)    for(int i = a; i < (b); ++i)
-#define rev(i, a,b)     for(int i = (b)-1; i >= a; --i)
+#define rev(i, a, b)    for(int i = (b)-1; i >= a; --i)
 #define trav(a, b)      for(auto& a : (b))
 #define all(a)          (a).begin(),(a).end()
 #define endl            '\n'
-#define INF             0x3f3f3f3f
+#define inf             0x3f3f3f3f
+#define M               1000000007
 
 using namespace std;
 using ll = int64_t;
+using u64 = uint64_t;
 using vi = vector<int>;
 using vvi = vector<vi>;
-using pii = pair<int, int>;
-using vpi = vector<pii>;
+using pi = pair<int, int>;
+using vpi = vector<pi>;
 
 int main() {
     ios::sync_with_stdio(false);
